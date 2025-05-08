87535ba88cd193f41dbab527786858803cd45b7b
AdjaRokhaya <adjarokhaya304@gmail.com>
2025-05-08 11:11:01 +0000

    Ajout du Lab9bis

--- test.c
+++ test.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+<<<<<<< HEAD
+=======
+int sum(int a, int b) {
+    return a + b;
+}
+
+>>>>>>> feature-sum
 int main (int argc,char const * argv[])
 {
     printf("TP DE GIT AVEC C\n");
